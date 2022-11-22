836452327cae60d7ff0a06f787cd9c5ed719da5e
李小凯 <lixiaokai@live.com>
2022-11-22 22:53:14 +0800

    完善：网站首页

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -1,19 +1,25 @@
 <?php
 
 declare(strict_types=1);
-/**
- * This file is part of Hyperf.
- *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
- */
+
 namespace App\Controller;
 
+use Hyperf\HttpServer\Annotation\Controller;
+use Hyperf\HttpServer\Annotation\GetMapping;
+
+/**
+ * 首页 - 控制器.
+ *
+ * @Controller(prefix="/")
+ */
 class IndexController extends AbstractController
 {
-    public function index()
+    /**
+     * 网站首页.
+     *
+     * @GetMapping(path="")
+     */
+    public function index(): array
     {
         $user = $this->request->input('user', 'Hyperf');
         $method = $this->request->getMethod();

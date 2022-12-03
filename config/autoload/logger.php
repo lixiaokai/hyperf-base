3fd2191004d5a7400ae3c296d72621109003403f
李小凯 <lixiaokai@live.com>
2022-12-04 00:52:24 +0800

    完善：日志配置

--- config/autoload/logger.php
+++ config/autoload/logger.php
@@ -7,21 +7,33 @@ declare(strict_types=1);
  *
  * @see https://hyperf.wiki/2.2/#/zh-cn/logger
  */
+
+$appEnv = env('APP_ENV', 'dev');
+
 return [
     'default' => [
-        'handler' => [
-            'class' => Monolog\Handler\StreamHandler::class,
-            'constructor' => [
-                'stream' => BASE_PATH . '/runtime/logs/hyperf.log',
-                'level' => Monolog\Logger::DEBUG,
+        'handlers' => [
+            // info、waring、notice 日志
+            [
+                'class' => Monolog\Handler\StreamHandler::class,
+                'constructor' => [
+                    'stream' => $appEnv === 'dev' ? 'php://stdout' : BASE_PATH . '/runtime/logs/hyperf.log',
+                    'level' => Monolog\Logger::INFO,
+                ],
+                'formatter' => [
+                    'class' => Monolog\Formatter\JsonFormatter::class,
+                ],
             ],
-        ],
-        'formatter' => [
-            'class' => Monolog\Formatter\LineFormatter::class,
-            'constructor' => [
-                'format' => null,
-                'dateFormat' => 'Y-m-d H:i:s',
-                'allowInlineLineBreaks' => true,
+            // error 日志
+            [
+                'class' => Monolog\Handler\StreamHandler::class,
+                'constructor' => [
+                    'stream' => $appEnv === 'dev' ? 'php://stdout' : BASE_PATH . '/runtime/logs/hyperf-error.log',
+                    'level' => Monolog\Logger::ERROR,
+                ],
+                'formatter' => [
+                    'class' => Monolog\Formatter\JsonFormatter::class,
+                ],
             ],
         ],
     ],

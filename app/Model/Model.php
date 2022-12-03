da2bcb17cd49354d31cb68c6cdf76f86e230c818
李小凯 <lixiaokai@live.com>
2022-12-03 23:45:40 +0800

    完善：模型基类

--- app/Model/Model.php
+++ app/Model/Model.php
@@ -1,21 +1,40 @@
 <?php
 
 declare(strict_types=1);
-/**
- * This file is part of Hyperf.
- *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
- */
+
 namespace App\Model;
 
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\Database\Model\Concerns\CamelCase;
 use Hyperf\DbConnection\Model\Model as BaseModel;
 use Hyperf\ModelCache\Cacheable;
 use Hyperf\ModelCache\CacheableInterface;
 
+/**
+ * 模型 - 抽象基类.
+ */
 abstract class Model extends BaseModel implements CacheableInterface
 {
     use Cacheable;
+    use CamelCase;
+
+    public static function column($col = '*'): string
+    {
+        $cols = (array) $col;
+        $tbName = self::tableName();
+
+        return collect($cols)->map(fn (string $col) => $tbName . '.' . $col)->implode(',');
+    }
+
+    public static function tableName(bool $withDb = false): string
+    {
+        $self = make(static::class);
+        $tbName = $self->getTable();
+        if ($withDb) {
+            $dbName = make(ConfigInterface::class)->get("databases.{$self->getConnectionName()}.database");
+            $tbName = $dbName . '.' . $tbName;
+        }
+
+        return $tbName;
+    }
 }
